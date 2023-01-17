b7641a4eb351916a0022e9bb55176bd6c515adf9
sarojson <sarojsontakke11@gmail.com>
2023-01-17 17:04:33 +0530

    commiting the project for the first time

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class LoginPage {
+	
+	public void getTitleOfPage()
+	{
+		System.out.println("this method provide the title page");
+	}
+
+}
---
+++ src/testclasses/LoginTest.java
@@ -0,0 +1,10 @@
+package testclasses;
+
+public class LoginTest {
+	
+	public void validateTitle()
+	{
+		System.out.println("Verifying the title of page");
+	}
+
+}
